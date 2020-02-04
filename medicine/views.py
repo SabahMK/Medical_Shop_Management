7aa0f6ecef2b25ad703e685bdb1a777ff77a3248
SabahMK <sabahmk03@gmail.com>
2020-02-04 19:43:29 +0530

    filter added

--- medicine/views.py
+++ medicine/views.py
@@ -2,22 +2,23 @@ from django.shortcuts import render
 from supplier.models import Supplier
 from employee.models import Employee
 from .models import Medicine
+from django.db.models import Q
 
 def med_home(request):
-    all_meds = Medicine.objects.all()
+    medicines = Medicine.objects.all()
     context = {
-        'all_meds' : all_meds,
+        'medicines' : medicines,
     }
 
     return render(request, 'main/medicine.html',context )
 
 def search_medicine(request):
-    if request.method == 'POST':
-        search_med = request.POST.get('search')
-    else:
-        search_med = ''
-    medicines = Medicine.objects.filter(name__icontains=search_med)
-    context = {
+    name = request.GET.get('q')
+    medicines = Medicine.objects.all().filter(
+        (Q(name__icontains=name) &
+        Q(quantity__icontains=name) &
+        Q(price__icontains=name))
+        context = {
         'medicines' : medicines,
     }
     return render(request, 'main/search_medicine.html', context)
